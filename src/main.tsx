f8b6c11246126c00d382d0ab858a99bf66d98990
bartik369 <6apmuk@gmail.com>
2025-07-13 18:40:33 +0300

    23

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import '@ant-design/v5-patch-for-react-19';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient.ts';
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/graphql-tanstack-styled">
+    <BrowserRouter>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import * as path from 'path';
 
 export default defineConfig({
-  base: "/graphql-tanstack-styled/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
