f56d19db8845575e853fba634f3f82cdd68cfb9b
bartik369 <6apmuk@gmail.com>
2025-07-13 19:18:37 +0300

    7

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,13 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient.ts';
 import App from './App.tsx'
 
+
+const redirectPath = sessionStorage.redirect;
+if (redirectPath) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, '', redirectPath);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
