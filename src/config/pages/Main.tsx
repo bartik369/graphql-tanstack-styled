2209dfac0388e6bfe59bc265a2e0f1d44ae93bb7
bartik369 <6apmuk@gmail.com>
2025-07-13 15:50:17 +0300

    added Home page

---
+++ src/config/pages/Main.tsx
@@ -0,0 +1,9 @@
+import Home from "@/components/pages/todo/home/Home";
+const HomeConfig = {
+    title: 'Home',
+    path: '/',
+    element: <Home/>,
+    requireAuth: false,
+}
+
+export default HomeConfig;
--- src/config/routes/publicRoutes.ts
+++ src/config/routes/publicRoutes.ts
@@ -1,6 +1,8 @@
 import TodosConfig from "../pages/Todos";
+import HomeConfig from "../pages/Main";
 
 const publicRoutes = [
     TodosConfig,
+    HomeConfig,
 ]
 export default publicRoutes;
