c1af7ab2d5244b07a4c12d4c8f6186ca2b90cb15
bartik369 <6apmuk@gmail.com>
2025-07-13 17:50:27 +0300

    12

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/graphql-tanstack-styled/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
