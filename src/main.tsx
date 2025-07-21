1151f172d6aac7e71add4ea519484f2c4ce20091
bartik369 <6apmuk@gmail.com>
2025-07-21 17:00:25 +0300

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,16 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient.ts';
 import App from './App.tsx'
 
-
-// const redirectPath = sessionStorage.redirect;
-// if (redirectPath) {
-//   sessionStorage.removeItem("redirect");
-//   window.history.replaceState(null, '', redirectPath);
-// }
+const redirectPath = sessionStorage.redirect;
+if (redirectPath) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, '', redirectPath);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <BrowserRouter basename="/graphql-tanstack-styled"> */}
-    <BrowserRouter>
+    <BrowserRouter basename="/graphql-tanstack-styled">
+    {/* <BrowserRouter> */}
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
--- src/shared/config/navigation.ts
+++ src/shared/config/navigation.ts
@@ -1,5 +1,5 @@
 export const NAVIGATION_ROUTES = [
-    { path: "/", label: "Главная" },
+    // { path: "/", label: "Главная" },
     { path: "/posts", label: "Посты" },
     { path: "/todos", label: "Задачи" },
   ];
